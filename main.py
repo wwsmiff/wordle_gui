02909f413fe54619ad1dcd2e08e913192dfceb5b
wwsmiff <arnav0872@gmail.com>
2023-01-15 23:09:35 +0530

    Added more features

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from tkinter import ttk
 from tkinter import messagebox
 import random
 import mysql.connector as mys
+from time import time
+import threading
+import collections
+from tkinter.simpledialog import askstring
 
 # Window dimensions
 WIDTH = 1024
@@ -11,10 +15,23 @@ HEIGHT = 720
 # Key States
 USED = "#242424"
 CORRECT = "#0f4200"
+EXISTS_IN_WORD = "#887400"
+
+DEBUG = True
+
+running = True
 
 tries = 0
 
 wordlist_data = []
+# Setting up MySQL
+db = mys.connect(user="root", password="batman", host="localhost")
+cursor = db.cursor()
+cursor.execute("create database if not exists cs;")
+cursor.execute("use cs;")
+cursor.execute("create table if not exists scores(name varchar(100), time_taken varchar(10), score decimal(5, 2));")
+
+username = askstring("Username", "What is your name?")
 
 # Setting up Tkinter
 root = Tk()
@@ -25,10 +42,15 @@ root.configure(background="#000000")
 keylist = []
 labels_list = []
 
+main_frame = Frame(root)
+timer_frame = Frame(root)
+
 user_input = Entry(root, width=5, bg="black", fg="white", font=("JetBrains Mono", 27, "bold"))
 user_input.place(relx=0.4, rely=0.9)
 
 random_word = ""
+
+score = 0
 
 keymap = {
     "A": (0, 0),
@@ -79,28 +101,47 @@ def init():
         wordlist_data[i] = wordlist_data[i].strip("\n")
 
     random_word = random.choice(wordlist_data)
+    letter_count = list(collections.Counter(random_word).values())
+    while any([i > 1 for i in letter_count]):
+        random_word = random.choice(wordlist_data)
+        letter_count = list(collections.Counter(random_word).values())
 
+    empty = Label(main_frame, text="", bg="#000000", height=3)
+    title = Label(main_frame, text="Wordle", font=("JetBrains Mono", 27, "bold"), bg="#000000", fg="white")
+    empty.grid(row=0, column=0)
+    title.place(x=350, y=5)
+
+    # Backup plan
+    # random_word_list = list(random_word)
+    # while True:
+    #     for (idx, _) in enumerate(random_word_list):
+    #         letter = random_word_list.pop(idx)
+    #         if letter in random_word_list:
+    #             random_word = random.choice(wordlist_data)
+    #             break
+    #     break
+
+    # Inputted words
     for i in range(6):
         tmp_label = []
         for j in range(5):
-            tmp_label.append(Label(root, text="", font=("JetBrains Mono", 27, "bold"), bg="#1f1f1f", fg="white", width=2, height=1))
+            tmp_label.append(Label(main_frame, text="", font=("JetBrains Mono", 27, "bold"), bg="#1f1f1f", fg="white", width=2, height=1))
             tmp_label[j].config(anchor=CENTER)
-            tmp_label[j].grid(row=len(labels_list), column=j, pady=(15, 0), padx=(0, 15))
-            tmp_label[0].grid(padx=(WIDTH - 900, 15))
+            tmp_label[j].grid(row=len(labels_list) + 1, column=j, pady=(15, 0), padx=(0, 15))
+            tmp_label[0].grid(padx=(WIDTH - 1020, 15))
         # Appending to the main list of inputted words.
         labels_list.append(tmp_label)
 
-    current_char = 65
 
+    # Keypad
     for i in range(7):
         tmp = []
         for j in range(4):
-            tmp.append(Label(text=inverse_keymap[(i, j)], font=("JetBrains Mono", 27), bg="#666666", fg="white", width=2, height=1))
+            tmp.append(Label(main_frame, text=inverse_keymap[(i, j)], font=("JetBrains Mono", 27), bg="#666666", fg="white", width=2, height=1))
             tmp[j].config(anchor=CENTER)
-            tmp[j].grid(row=i, column=j+5, pady=(15, 0), padx=(0, 15))
+            tmp[j].grid(row=i + 1, column=j+5, pady=(15, 0), padx=(0, 15))
             tmp[0].grid(padx=(WIDTH // 4, 15))
 
-            current_char += 1
         keylist.append(tmp)
 
 def add_word(self):
@@ -109,42 +150,73 @@ def add_word(self):
     global tries
     global wordlist_data
 
-    tmp_label = labels_list[tries]
+    try:
 
-    # Checking the size of the inputted word
-    if len(user_input.get()) != 5:
-        messagebox.showwarning("", "Word must be 5 letters long")
+        tmp_label = labels_list[tries]
 
-    elif user_input.get() not in wordlist_data:
-        messagebox.showwarning("", "Word not in word list")
+        # Checking the size of the inputted word
+        if len(user_input.get()) != 5:
+            messagebox.showwarning("", "Word must be 5 letters long")
 
-    # Checking if the inputted word is right
-    elif user_input.get() == random_word:
-        for i in range(5):
-            tmp_label[i].config(text=user_input.get()[i].upper())
-            tmp_label[i].config(bg="#0f4200")
-    else:
-        for i in range(5):
-            # Appending characters to display with colours
-            if user_input.get()[i] in random_word and i == random_word.index(user_input.get()[i]):
+        elif user_input.get() not in wordlist_data:
+            messagebox.showwarning("", "Word not in word list")
+
+        # Checking if the inputted word is right
+        elif user_input.get() == random_word:
+            for i in range(5):
                 tmp_label[i].config(text=user_input.get()[i].upper())
                 tmp_label[i].config(bg="#0f4200")
-                update_keyboard(user_input.get()[i].upper(), CORRECT)
+            
+            messagebox.showinfo("You won")
+            exit()
 
-            elif user_input.get()[i] in random_word:
-                tmp_label[i].config(text=user_input.get()[i].upper())
-                tmp_label[i].config(bg="#887400")
-                update_keyboard(user_input.get()[i].upper(), USED)
-            else:
-                tmp_label[i].config(text=user_input.get()[i].upper())
-                tmp_label[i].config(bg="#1f1f1f")
-                update_keyboard(user_input.get()[i].upper(), USED)
+            return
 
-        # labels_list.append(tmp_label)
-        tries += 1
+        else:
+            for i in range(5):
+                # Appending characters to display with colours
+                if user_input.get()[i] in random_word and i == random_word.index(user_input.get()[i]):
+                    tmp_label[i].config(text=user_input.get()[i].upper())
+                    tmp_label[i].config(bg="#0f4200")
+                    update_keyboard(user_input.get()[i].upper(), CORRECT)
 
-    # Clearing the input
-    user_input.delete(0, 'end')
+                elif user_input.get()[i] in random_word:
+                    tmp_label[i].config(text=user_input.get()[i].upper())
+                    tmp_label[i].config(bg="#887400")
+                    update_keyboard(user_input.get()[i].upper(), EXISTS_IN_WORD)
+                else:
+                    tmp_label[i].config(text=user_input.get()[i].upper())
+                    tmp_label[i].config(bg="#1f1f1f")
+                    update_keyboard(user_input.get()[i].upper(), USED)
+
+            # labels_list.append(tmp_label)
+            tries += 1
+
+        if tries >= 6:
+            messagebox.info("You're out of tries!")
+            exit()
+
+        # Clearing the input
+        user_input.delete(0, 'end')
+
+    except:
+        exit()
+
+def exit():
+    global username
+    global time_elapsed_str
+    global db
+    global cursor
+    global running
+
+    if running:
+        score = ((6 - tries) / 6) * 100
+        cursor.execute("insert into scores values('{}', '{}', {})".format(username, time_elapsed_str, score))
+        db.commit()
+
+    running = False
+
+
 
 def update_keyboard(letter, state):
     global user_input
@@ -155,13 +227,48 @@ def update_keyboard(letter, state):
 
     keylist[r][c].config(bg = state)
 
-    print(letter)
-    print(f"{r = }, {c = }")
+def update_timer():
+    global running
+    global time_elapsed_str
+
+    start_time = time()
+    timer_label = Label(root, text="00:00:00", font=("JetBrains Mono", 27), bg="#000000", fg="white")
+    timer_label.place(x = 390, y = 570)
+    hrs = 0
+    mins = 0
+    seconds = 0
+
+    while True:
+        seconds = int(time() - start_time)
+        time_elapsed_str = "{:02d}:{:02d}:{:02d}".format(hrs, mins, seconds)
+        timer_label.config(text = time_elapsed_str)
+        if seconds >= 60:
+            mins += 1
+            seconds = 0
+            start_time = time()
+
+        if mins >= 60:
+            mins = 0
+            hrs += 1
+            seconds = 0
+            start_time = time()
+
+        if running == False:
+            break
 
 if __name__ == "__main__":
     init()
-    print(random_word)
+    if DEBUG:
+        print(random_word)
     user_input.focus_set()
-    # Binding a callback function to the return key
+    main_frame.config(bg="#000000")
+    timer_frame.config(bg="#000000")
+    main_frame.pack()
+    # timer_frame.pack()
+    # Binding a callback function to the Enter/Return key
     root.bind("<Return>", add_word)
+    timer_thread = threading.Thread(target=update_timer)
+    timer_thread.start()
     root.mainloop()
+    running = False
+    timer_thread.join()
